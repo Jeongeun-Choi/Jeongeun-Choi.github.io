5315168d9bee99840f76c030264f7be97ce487d3
Jeongeun-Choi <cje3080@gmail.com>
2022-03-01 18:24:38 +0900

    gatsby로 블로그 만들기

---
+++ src/components/Main/PostList.tsx
@@ -0,0 +1,60 @@
+import styled from '@emotion/styled';
+import React, { FunctionComponent, useMemo } from 'react';
+import PostItem from './PostItem';
+import { PostListItemType } from 'types/PostItem.types';
+
+const POST_ITEM_DATA = {
+  title: 'Post Item Title',
+  date: '2022.02.23',
+  categories: ['Web', 'Frontend', 'Testing'],
+  summary:
+    '안녕하세요. 최정은입니다. 아 진짜 귀찮다 로또 당첨됐으면 좋겠다. 그럼 이만.',
+  thumbnail:
+    'https://img1.daumcdn.net/thumb/R1280x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/cnoC/image/4yPtuRXtR0-jusOMCCXb4MeN6zU.jpg',
+  link: 'www.naver.com',
+};
+
+type PostListProps = {
+  selectedCategory: string;
+  posts: PostListItemType[];
+};
+
+const PostList: FunctionComponent<PostListProps> = ({
+  selectedCategory,
+  posts,
+}) => {
+  const filterPosts = useMemo(() => {
+    if (selectedCategory === 'All') {
+      return posts;
+    } else {
+      return posts.filter(post =>
+        post.node.frontmatter.categories.includes(selectedCategory),
+      );
+    }
+  }, [selectedCategory]);
+
+  return (
+    <PostListWrapper>
+      {filterPosts.map(({ node: { id, frontmatter } }) => (
+        <PostItem {...frontmatter} link="www.naver.com" key={id} />
+      ))}
+    </PostListWrapper>
+  );
+};
+
+export default PostList;
+
+const PostListWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 20px;
+  width: 768px;
+  margin: 0 auto;
+  padding: 50px 0 100px;
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+    width: 100%;
+    padding: 50px 20px;
+  }
+`;
